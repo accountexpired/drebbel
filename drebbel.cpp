64899a1a492c37042bf87bd311762a3bde583d7b
dhf <daghenrik@gmail.com>
2016-10-09 18:50:05 +0200

    Add input handling.

--- drebbel.cpp
+++ drebbel.cpp
@@ -1,24 +1,74 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+
+#include "drebbel.hpp"
+
+void handle_input(sf::Event& event, sf::RenderWindow& window, sf::Sprite& sprite)
+{
+    if (event.type == sf::Event::Closed)
+    {
+        window.close();
+    }
+    else if (event.type == sf::Event::KeyPressed)
+    {
+        if (event.key.code == sf::Keyboard::Left)
+        {
+            sprite.move(sf::Vector2f(-10, 0));
+        }
+        else if (event.key.code == sf::Keyboard::Right)
+        {
+            sprite.move(sf::Vector2f(10, 0));
+        }
+        else if (event.key.code == sf::Keyboard::Up)
+        {
+            sprite.move(sf::Vector2f(0, -10));
+        }
+        else if (event.key.code == sf::Keyboard::Down)
+        {
+            sprite.move(sf::Vector2f(0, 10));
+        }
+    }
+}
+
+bool load_sprite(sf::Texture& texture, const std::string& texture_path, sf::Sprite& sprite)
+{
+    if (!texture.loadFromFile(texture_path))
+    {
+        std::cerr << "Unable to load texture: %s" << texture_path << std::endl;
+        return false;
+    }
+    texture.setSmooth(true);
+    sprite.setTexture(texture);
+
+    return true;
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Drebbel");
+
+    sf::Texture sub_texture;
+    sf::Sprite sub;
+    load_sprite(sub_texture, "images/submarine.png", sub);
+
+    sf::Texture iceberg_texture;
+    sf::Sprite iceberg;
+    load_sprite(iceberg_texture, "images/iceberg.png", iceberg);
+
+    iceberg.setPosition(100, 300);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+            handle_input(event, window, sub);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+            window.clear(sf::Color::Blue);
+            window.draw(sub);
+            window.draw(iceberg);
+            window.display();
+        }
     }
 }
---
+++ drebbel.hpp
@@ -0,0 +1,6 @@
+#ifndef DREBBEL_H
+#define DREBBEL_H
+
+void handle_input(sf::Event& event, sf::RenderWindow& window, sf::Sprite& sprite);
+
+#endif
