2ef85c28b9be4b22ee314ccb151854dcaa71763c
dhf <daghenrik@gmail.com>
2016-10-16 19:51:16 +0200

    Update input handling and do some refactoring.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 drebbel: drebbel.o
-	clang++ drebbel.o -o drebbel -lsfml-graphics -lsfml-window -lsfml-system
+	clang++ drebbel.o -o drebbel -lsfml-graphics -lsfml-window -lsfml-system -std=c++14
 
 drebbel.o: drebbel.cpp
-	clang++ -c drebbel.cpp
+	clang++ -c drebbel.cpp -std=c++14
--- drebbel.cpp
+++ drebbel.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
+#include <memory>
 
 #include <SFML/Graphics.hpp>
 
 #include "drebbel.hpp"
 
-void handle_input(sf::Event& event, sf::RenderWindow& window, sf::Sprite& sprite)
+namespace
 {
-    if (event.type == sf::Event::Closed)
+    namespace Local
     {
-        window.close();
-    }
-    else if (event.type == sf::Event::KeyPressed)
-    {
-        if (event.key.code == sf::Keyboard::Left)
-        {
-            sprite.move(sf::Vector2f(-10, 0));
-        }
-        else if (event.key.code == sf::Keyboard::Right)
-        {
-            sprite.move(sf::Vector2f(10, 0));
-        }
-        else if (event.key.code == sf::Keyboard::Up)
-        {
-            sprite.move(sf::Vector2f(0, -10));
-        }
-        else if (event.key.code == sf::Keyboard::Down)
-        {
-            sprite.move(sf::Vector2f(0, 10));
-        }
+        const int window_x_res = 800;
+        const int window_y_res = 600;
     }
 }
 
@@ -44,30 +27,139 @@ bool load_sprite(sf::Texture& texture, const std::string& texture_path, sf::Spri
     return true;
 }
 
+class Entity
+{
+protected:
+    int x_coord;
+    int y_coord;
+    sf::Texture texture;
+    sf::Sprite sprite;
+
+public:
+    Entity(const std::string& image, const int x_pos, const int y_pos);
+    virtual void move(int x_coord, int y_coord);
+    sf::Sprite& get_sprite();
+};
+
+Entity::Entity(const std::string& image, const int x_pos, const int y_pos)
+{
+    load_sprite(texture, image, sprite);
+    sprite.setPosition(x_pos, y_pos);
+    x_coord = x_pos;
+    y_coord = y_pos;
+}
+
+void Entity::move(int x_coord, int y_coord)
+{
+    sprite.move(sf::Vector2f(x_coord, y_coord));
+}
+
+sf::Sprite& Entity::get_sprite()
+{
+    return sprite;
+}
+
+class Submarine : public Entity
+{
+    using Entity::Entity;
+
+public:
+    void move(int x_coord, int y_coord);
+};
+
+void Submarine::move(int x_coord, int y_coord)
+{
+}
+
+class Mountain : public Entity
+{
+    using Entity::Entity;
+};
+
+class World
+{
+    std::vector<std::unique_ptr<Entity>> entities;
+public:
+    void add_entity(std::unique_ptr<Entity>);
+    void draw_entities(sf::RenderWindow& window) const;
+    void move(const int x_coord, const int y_coord);
+};
+
+void World::add_entity(std::unique_ptr<Entity> entity)
+{
+    entities.push_back(std::move(entity));
+}
+
+void World::draw_entities(sf::RenderWindow& window) const
+{
+    for (const auto& entity : entities)
+    {
+        window.draw(entity->get_sprite());
+    }
+}
+
+void World::move(const int x_coord, const int y_coord)
+{
+    for (const auto& entity : entities)
+    {
+        entity->move(x_coord, y_coord);
+    }
+}
+
+void handle_input(sf::Event& event, sf::RenderWindow& window, World& world)
+{
+    if (event.type == sf::Event::Closed)
+    {
+        window.close();
+    }
+
+    else if (event.type == sf::Event::KeyPressed)
+    {
+        if (event.key.code == sf::Keyboard::Left)
+        {
+            world.move(10, 0);
+        }
+        else if (event.key.code == sf::Keyboard::Right)
+        {
+            world.move(-10, 0);
+        }
+        else if (event.key.code == sf::Keyboard::Up)
+        {
+            world.move(0, 10);
+        }
+        else if (event.key.code == sf::Keyboard::Down)
+        {
+            world.move(0, -10);
+        }
+    }
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Drebbel");
+    sf::RenderWindow window(sf::VideoMode(Local::window_x_res, Local::window_y_res), "Drebbel");
 
-    sf::Texture sub_texture;
-    sf::Sprite sub;
-    load_sprite(sub_texture, "images/submarine.png", sub);
+    World world;
 
-    sf::Texture iceberg_texture;
-    sf::Sprite iceberg;
-    load_sprite(iceberg_texture, "images/iceberg.png", iceberg);
+    // Position the submarine a slightly left of the center of the window.
+    auto sub = std::make_unique<Submarine>(
+        "images/submarine.png",
+        Local::window_x_res / 2 - Local::window_x_res * 0.1,
+        Local::window_y_res / 2);
 
-    iceberg.setPosition(100, 300);
+    auto iceberg = std::make_unique<Mountain>("images/iceberg.png", 100, 300);
+
+    world.add_entity(std::move(sub));
+    world.add_entity(std::move(iceberg));
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            handle_input(event, window, sub);
+            handle_input(event, window, world);
 
             window.clear(sf::Color::Blue);
-            window.draw(sub);
-            window.draw(iceberg);
+            world.draw_entities(window);
             window.display();
         }
     }
